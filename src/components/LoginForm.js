67a1101fdf9f0ed96fa3a45e24614b329faebbd0
Eric <ericezee@gmail.com>
2018-06-07 19:54:48 -0700

    Lesson 69

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-    state = { email: '' };
+    state = { email: '', password: '' };
+
+    onButtonPress() {
+    const { email, password } = this.state;
+        firebase.auth().signInWithEmailAndPassword(email, password);
+    }
 
     render() {
         return (
@@ -10,16 +16,24 @@ class LoginForm extends Component {
                 <CardSection>
                     <Input
                         placeholder="user@gmail.com"
-                        label="Email:"
+                        label="Email"
                         value={this.state.email}
                         onChangeText={email => this.setState({ email })}
                     />
                 </CardSection>
 
-                <CardSection />
+                <CardSection>
+                    <Input
+                        placeholder="password"
+                        label="Password"
+                        value={this.state.password}
+                        onChangeText={password => this.setState({ password })}
+                        secureTextEntry={true}
+                    />
+                </CardSection>
 
                 <CardSection>
-                    <Button>
+                    <Button onPress={this.onButtonPress.bind(this)}>
                         LOGIN
                     </Button>
                 </CardSection>
